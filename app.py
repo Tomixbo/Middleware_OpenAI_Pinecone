d26f58e42cea132f926872eb7afec83505cc4b8b
Tomixbo <tomixbo@gmail.com>
2024-05-30 14:33:34 +0300

    adding endpoints : kate, cleon

--- app.py
+++ app.py
@@ -20,11 +20,15 @@ pinecone_api_key = os.getenv("PINECONE_API_KEY")
 environment = os.getenv("PINECONE_ENV")
 index_hugo_name = os.getenv("PINECONE_INDEX_HUGO")
 index_marie_name = os.getenv("PINECONE_INDEX_MARIE")
+index_kate_name = os.getenv("PINECONE_INDEX_KATE")
+index_cleon_name = os.getenv("PINECONE_INDEX_CLEON")
 
 # Initialize pinecone client
 pc = Pinecone(api_key=os.getenv(pinecone_api_key))
 index_hugo = pc.Index(index_hugo_name)
 index_marie = pc.Index(index_marie_name)
+index_kate = pc.Index(index_kate_name)
+index_cleon = pc.Index(index_cleon_name)
 
 
 # Middleware to secure HTTP endpoint
@@ -79,3 +83,36 @@ async def get_context_marie(
     # Retrun context
     return context
 
+@app.post("/kate/")
+async def get_context_kate(
+    query_data: QueryModel,
+    credentials: HTTPAuthorizationCredentials = Depends(validate_token),
+):
+    # convert query to embeddings
+    res = openai_client.embeddings.create(
+        input=[query_data.query], model="text-embedding-3-small"
+    )
+    embedding = res.data[0].embedding
+    # Search for matching Vectors
+    results = index_kate.query(vector=embedding, top_k=3, include_metadata=True).to_dict()
+    # Filter out metadata fron search result
+    context = [match["metadata"]["text"] for match in results["matches"]]
+    # Retrun context
+    return context
+
+@app.post("/cleon/")
+async def get_context_cleon(
+    query_data: QueryModel,
+    credentials: HTTPAuthorizationCredentials = Depends(validate_token),
+):
+    # convert query to embeddings
+    res = openai_client.embeddings.create(
+        input=[query_data.query], model="text-embedding-3-small"
+    )
+    embedding = res.data[0].embedding
+    # Search for matching Vectors
+    results = index_cleon.query(vector=embedding, top_k=3, include_metadata=True).to_dict()
+    # Filter out metadata fron search result
+    context = [match["metadata"]["text"] for match in results["matches"]]
+    # Retrun context
+    return context
