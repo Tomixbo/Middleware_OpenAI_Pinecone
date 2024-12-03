fcba6331f88fe9b85357fa468068356e54ff74de
Tomixbo <tomixbo@gmail.com>
2024-12-03 17:25:29 +0300

    update app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pinecone
 from threading import Thread
 
 def run():
-    uvicorn.run("app:app", host="0.0.0.0", port=10000, reload=True)
+    app.run(host="0.0.0.0", port=10000)
 
 def keep_alive():
   t = Thread(target=run)
